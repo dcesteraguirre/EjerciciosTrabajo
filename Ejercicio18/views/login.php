bc4fe706f45b50d83a3c10b542c9353af4f6b61d
dcesteraguirre <113922135+dcesteraguirre@users.noreply.github.com>
2022-11-05 23:59:24 +0100

    Update login.php

--- Ejercicio18/views/login.php
+++ Ejercicio18/views/login.php
@@ -5,7 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
 </head>
-  <bdy>
+  <body>
     <h1>Formulario</h1>
     <form action="?method=confirmacion" method="post">
       <label for="">Nombre:  </label>
